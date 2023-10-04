6009525cf3f26f782621a590afa39550684fd6ae
unknown <jaiswalsanket244@gmail.com>
2023-10-04 15:50:14 +0530

    first push

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// resume button 
+// let btn = document.querySelector("#resume-button-1")
+// let btn2=document.querySelector("#resume-button-2")
+// btn2.addEventListener("click",redirec)
+// btn.addEventListener("click",redirec)
+
+
+function redirec(){
+
+window.open("https://drive.google.com/file/d/14cpb-yIeYybLQTfq981lfUEBUu_N8i17/view?usp=share_link","blank")
+
+}
+
+// resume button 
+
+
+GitHubCalendar(".calendar", "jaysukla", { responsive: true });
+
+
+
+
+
+
+
+
